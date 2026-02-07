c859ae365fcefd5303b5c8e7487650d6caa998c4
Luxury Leads AI <yasar.ai.leads@gmail.com>
2026-02-07 18:47:21 +0500

    Force DB init on startup

--- app.py
+++ app.py
@@ -203,9 +203,9 @@ def export_leads(agency_id):
 # -------------------------
 # INIT
 # -------------------------
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-    app.run(debug=True)
+with app.app_context():
+    db.create_all()
